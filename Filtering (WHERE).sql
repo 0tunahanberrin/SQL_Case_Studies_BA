12d4972b9080bbd4a80bafdc044f2bdc65818a31
Berrin Tunahan <94645801+0tunahanberrin@users.noreply.github.com>
2025-11-27 21:11:21 +0300

    Update Filtering (WHERE).sql

--- Filtering (WHERE).sql
+++ Filtering (WHERE).sql
@@ -18,3 +18,38 @@ WHERE
     )
 ORDER BY
     departmentid;
+
+--How many employees joined in January month.--
+SELECT
+	COUNT(*) AS January_employees
+FROM EMPLOYEE
+WHERE
+	DATE_PART('month', to_date(hiredate, 'DD-MON-YY')) = 1;
+
+--How many employees joined in January or September month.--
+SELECT 
+
+COUNT(CASE WHEN EXTRACT(MONTH FROM to_date(hiredate, 'DD-MON-YY')) = 1 THEN 1 END) AS January_employees, 
+COUNT(CASE WHEN EXTRACT(MONTH FROM to_date(hiredate, 'DD-MON-YY')) = 9 THEN 1 END) AS September_employees 
+ 
+FROM EMPLOYEE; 
+
+--How many employees joined in 1985.--
+SELECT
+	COUNT(*) AS joined_in_1985_employees	
+FROM EMPLOYEE
+WHERE
+	EXTRACT(YEAR FROM to_date(hiredate, 'DD-MON-YY')) = 1985;
+
+--How many employees joined each month in 1985.--
+SELECT
+	EXTRACT(MONTH FROM to_date(hiredate, 'DD-MON-YY')) AS month, 
+	COUNT(*) AS employee_count
+FROM EMPLOYEE
+WHERE
+	EXTRACT(YEAR FROM to_date(hiredate, 'DD-MON-YY')) = 1985
+GROUP BY
+	month
+ORDER BY
+	month ASC;
+	
