376d682443b43140203bf98e8a749bd192cd4eac
Berrin Tunahan <94645801+0tunahanberrin@users.noreply.github.com>
2026-04-26 15:26:24 +0300

    Update JOIN.sql

--- JOIN.sql
+++ JOIN.sql
@@ -16,4 +16,36 @@ FROM EMPLOYEE
 JOIN DEPARTMENT ON EMPLOYEE.departmentID = DEPARTMENT.departmentID 
 WHERE DEPARTMENT.dept_name = 'SALES'; 
 
+--List all departments having greater than or equal to 2 employees and display the department names in ascending order.--
+SELECT 
 
+    	dept_name, 
+
+	COUNT(EMPLOYEE.employeeID) AS employee_count 
+
+FROM EMPLOYEE 
+
+JOIN 
+
+	DEPARTMENT ON EMPLOYEE.departmentID = DEPARTMENT.departmentID 
+
+GROUP BY 
+
+	dept_name 
+
+HAVING COUNT(EMPLOYEE.employeeID) >= 2 
+
+ORDER BY DEPARTMENT.dept_name ASC;
+
+--Display employee count for each designation within the organization.--
+SELECT
+	JOB.job_name AS designation,
+	COUNT(EMPLOYEE.employeeID) AS employees_count
+FROM
+	EMPLOYEE
+JOIN
+	JOB ON EMPLOYEE.jobID = JOB.jobID
+GROUP BY
+	JOB.job_name
+ORDER BY
+	employees_count DESC;*/
